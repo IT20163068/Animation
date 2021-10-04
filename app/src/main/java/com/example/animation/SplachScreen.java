020c2b563a0caeca6fca3243d832b7ec0e901c1c
Punsisi Jayasundera <it20163068@my.sliit.lk>
2021-10-05 01:29:56 +0530

    Animation of Android_Studio

---
+++ app/src/main/java/com/example/animation/SplachScreen.java
@@ -0,0 +1,38 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplachScreen extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+
+        appname = findViewById(R.id.appname);
+        lottie = findViewById(R.id.lottie);
+
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2700).setDuration(2700).setStartDelay(3600);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        },5000);
+    }
+}
